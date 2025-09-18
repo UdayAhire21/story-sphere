f0fb8ba6532e9084fc10a722ff6993cfb9480f2a
UdayAhire21 <udayahire27feb@gmail.com>
2025-09-18 20:03:49 +0530

    nc

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navigation />
+        <Navigation/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/library" element={<Library />} />
